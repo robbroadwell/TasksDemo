bf543addb7528cffda1bb33b9fa7378cad67f4e1
Rob Broadwell <rob@inspiringapps.com>
2023-01-01 16:48:22 -0500

    Initial Commit

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Rob Broadwell on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
